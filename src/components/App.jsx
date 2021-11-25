73057519134e1bd7a8759310627b1b35acb5636b
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-25 17:05:33 +0100

    Create a rendering function in order to render login page based on a condition.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
+let isLoggedIn = true;
+
+function rendering() {
+    if (isLoggedIn) {
+        return (
+            <h1>Hello</h1>
+        );
+    } else {
+        return (
+            <form className="form">
+                <input type="text" placeholder="Username"/>
+                <input type="password" placeholder="Password"/>
+                <button type="submit">Login</button>
+            </form>
+        );
+    }
+}
+
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+          {rendering()}
+        </div>
+    );
 }
 
 export default App;
