b706ce56d7cc64fdda2872ac15ed676d1a24676e
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-25 17:42:28 +0100

    Turn rendering function into inline Ternary Operator.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Login from "./Login";
 
-let isLoggedIn = false;
-
-function rendering() {
-    if (isLoggedIn) {
-        return (
-            <h1>Hello</h1>
-        );
-    } else {
-        return (<Login/>);
-    }
-}
+let isLoggedIn = true;
 
 function App() {
     return (
         <div className="container">
-            {rendering()}
+            {isLoggedIn?  <h1>Hello</h1> : <Login/>}
         </div>
     );
 }
