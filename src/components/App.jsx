6e6644de67078d57f089ab0528257ab6005116d3
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-25 17:56:55 +0100

    Final commit.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,8 @@ console.log(currentTime);
 function App() {
     return (
         <div className="container">
-            {/* {isLoggedIn?  <h1>Hello</h1> : <Login/>} */}
-            {currentTime > 16 && <h1>Why you still working? go have some fun.</h1> }
+            {isLoggedIn?  <h1>Hello</h1> : <Login/>}
+            {/* {currentTime > 16 && <h1>Why you still working? go have some fun.</h1> } */}
             
         </div>
     );
